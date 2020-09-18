529ad3b55193b795d09f980d09f0faa31832a904
andimeon <andimeon@gmail.com>
2020-09-18 18:07:37 +0900

    Add serializers

---
+++ foodcartapp/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from foodcartapp.models import Order, OrderProduct
+
+
+class OrderProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderProduct
+        fields = ['product', 'quantity']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    products = OrderProductSerializer(many=True, write_only=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', 'firstname', 'lastname', 'phonenumber', 'address', 'products']
