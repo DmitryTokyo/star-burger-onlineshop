4fe644c6018aa6b1b618cb9ce6aa2856e2c7ed50
andimeon <andimeon@gmail.com>
2020-09-20 12:09:25 +0900

    prefetch OrderProducts objects

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -99,7 +99,7 @@ def view_restaurants(request):
 
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
-    orders = Order.objects.all()
+    orders = Order.objects.all().prefetch_related('products')
 
     order_items = []
     for order in orders:
