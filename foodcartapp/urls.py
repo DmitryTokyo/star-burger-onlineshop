d1d717d1f05617e2b9d0e18a8b0949e626b2b2db
andimeon <andimeon@gmail.com>
2020-10-19 21:47:30 +0900

    Update api url

--- foodcartapp/urls.py
+++ foodcartapp/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .views import product_list_api, banners_list_api, register_order
 
@@ -7,6 +9,6 @@ app_name = "foodcartapp"
 
 urlpatterns = [
     path('products/', product_list_api),
-    path('banners/', banners_list_api),
+    path('banners/start-page-header/', banners_list_api),
     path('order/', register_order),
 ]
