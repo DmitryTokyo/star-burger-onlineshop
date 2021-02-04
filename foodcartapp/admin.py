df680c3704b616447414e163484232b47113849a
andimeon <andimeon@gmail.com>
2021-02-04 15:08:59 +0900

    Fix ingredients field

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -71,7 +71,7 @@ class ProductAdmin(admin.ModelAdmin):
         ('Подробно', {
             'fields': [
                 'special_status',
-                'ingridients',
+                'ingredients',
             ],
             'classes': [
                 'wide'
