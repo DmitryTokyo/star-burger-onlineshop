e5e69881e34df4fe22c3f6f466769aa1835e37c5
andimeon <andimeon@gmail.com>
2022-07-25 14:00:30 +0900

    Add DATABASE schema url to settings.py

--- StarBurger/settings.py
+++ StarBurger/settings.py
@@ -113,7 +113,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 DATABASES = {
-    'default': dj_database_url.config(
+    'default': env.db_url(
+        'DATABASE_URL',
         default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3')),
     ),
 }
